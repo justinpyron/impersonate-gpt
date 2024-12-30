46368099c0ebd12ecb84d1bea78244c6e8627f43
Justin Pyron <justinpyron@gmail.com>
2024-12-30 17:04:57 -0600

    lint

--- impersonate_dataset.py
+++ impersonate_dataset.py
@@ -4,7 +4,7 @@ from torch.utils.data import Dataset
 def process_chunk(chunk: str) -> str:
     first_space = chunk.find(" ")
     last_space = chunk.rfind(" ")
-    return chunk[first_space+1: last_space]
+    return chunk[first_space + 1 : last_space]
 
 
 def make_chunks(
@@ -15,16 +15,15 @@ def make_chunks(
     text = "".join(text.splitlines(keepends=True)[skip_lines:])
     chunks = list()
     i = 0
-    while i < len(text) - int(1.5*length):
-        chunks.append(text[i : i+length])
-        chunks.append(text[int(i + 0.5*length) : int(i + 1.5*length)])
+    while i < len(text) - int(1.5 * length):
+        chunks.append(text[i : i + length])
+        chunks.append(text[int(i + 0.5 * length) : int(i + 1.5 * length)])
         i += length
     chunks = [process_chunk(c) for c in chunks]
     return chunks
 
 
 class ImposterDataset(Dataset):
-
     def __init__(
         self,
         file_paths: str,
@@ -39,7 +38,7 @@ class ImposterDataset(Dataset):
             self.chunks += [
                 tokenizer(c, return_tensors="pt")["input_ids"][0] for c in chunk
             ]
-        
+
     def __len__(self):
         return len(self.chunks)
 
