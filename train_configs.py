34273b10bac400c93927a16fe9beefd19d9cdf1d
Justin Pyron <justinpyron@gmail.com>
2024-12-31 17:53:42 -0600

    Add Darwin to train configs

--- train_configs.py
+++ train_configs.py
@@ -8,15 +8,15 @@ class ImpersonateConfig:
     files_eval: list[str]
 
 
-config_fitzgerald = ImpersonateConfig(
-    name="fitzgerald",
+config_darwin = ImpersonateConfig(
+    name="darwin",
     files_train=[
-        "data/fitzgerald-beautiful_and_damned.txt",
-        "data/fitzgerald-flappers_and_philosophers.txt",
-        "data/fitzgerald-great_gatsby.txt",
-        "data/fitzgerald-tales_of_the_jazz_age.txt",
+        "data/darwin-coral_reefs.txt",
+        "data/darwin-descent_of_man.txt",
+        "data/darwin-origin_of_species.txt",
+        "data/darwin-voyage_of_the_beagle.txt",
     ],
-    files_eval=["data/fitzgerald-this_side_of_paradise.txt"],
+    files_eval=["data/darwin-emotions_in_man_and_animals.txt"],
 )
 
 config_dostoevsky = ImpersonateConfig(
@@ -30,14 +30,26 @@ config_dostoevsky = ImpersonateConfig(
     files_eval=["data/dostoevsky-the_idiot.txt"],
 )
 
+config_fitzgerald = ImpersonateConfig(
+    name="fitzgerald",
+    files_train=[
+        "data/fitzgerald-beautiful_and_damned.txt",
+        "data/fitzgerald-flappers_and_philosophers.txt",
+        "data/fitzgerald-great_gatsby.txt",
+        "data/fitzgerald-tales_of_the_jazz_age.txt",
+    ],
+    files_eval=["data/fitzgerald-this_side_of_paradise.txt"],
+)
+
 config_twain = ImpersonateConfig(
     name="twain",
     files_train=[
-        "data/twain-connecticut_yankee_king_arthurs_court.txt",
         "data/twain-the_prince_and_the_pauper.txt",
         "data/twain-tom_sawyer.txt",
+        "data/twain-huckleberry_finn.txt",
+        "data/twain-life_on_the_mississippi.txt",
     ],
-    files_eval=["data/twain-huckleberry_finn.txt"],
+    files_eval=["data/twain-connecticut_yankee_king_arthurs_court.txt"],
 )
 
 configs = {
